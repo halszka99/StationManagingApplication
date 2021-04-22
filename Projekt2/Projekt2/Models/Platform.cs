b48f3a9f1fe197458e29c65977300bc4fc8426e6
halszka99 <halszka99@interia.eu>
2021-04-22 14:54:36 +0200

    Classes with properties

---
+++ Projekt2/Projekt2/Models/Platform.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt2.Models
+{
+    class Platform
+    {
+        public int Id { get; set; }
+        public Track TrackTop { get; set; }
+        public Track TrackDown { get; set; }
+
+    }
+}
