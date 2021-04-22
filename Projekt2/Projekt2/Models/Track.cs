ba8eeb04eb3f94e32887e643e0b2cde0cc2aad95
halszka99 <halszka99@interia.eu>
2021-04-22 14:54:36 +0200

    Classes with properties

---
+++ Projekt2/Projekt2/Models/Track.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt2.Models
+{
+    class Track
+    {
+        public int Id { get; set; }
+        public bool IsEmpty { get; set; }
+    }
+}
---
+++ Projekt2/Projekt2/Models/Train.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt2.Models
+{
+    class Train
+    {
+        public int Id { get; set; }
+        public EntryTrack EntryTrack { get; set; }
+        public EntryTrack ExitTrack { get; set; }
+        public Track DestinationTrack { get; set; }
+        public Track CurrentTrack { get; set; }
+
+    }
+}
