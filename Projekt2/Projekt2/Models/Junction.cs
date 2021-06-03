d0bf1f9c40500dbc84278e41cdee96e0fea58ea9
halszka99 <halszka99@interia.eu>
2021-06-03 18:50:43 +0200

    empty metohods added

--- Projekt2/Projekt2/Models/Junction.cs
+++ Projekt2/Projekt2/Models/Junction.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Projekt2.Models
@@ -9,5 +10,19 @@ namespace Projekt2.Models
     class Junction
     {
         public List<Track> EntryTracks { get; set; }
+        public bool IsEmpty { get; set; }
+        Mutex JunctionMutex = new Mutex();
+        public Junction()
+        {
+            // tu będziemy tworzyć entry tracki do junctiona
+        }
+        public void Reserve()
+        {
+            // tu będziemy wprowadzać na junction do wjazdu
+        }
+        public void Free()
+        {
+            // tu będziemy wprowadzać na junction do wjazdu
+        }
     }
 }
--- Projekt2/Projekt2/Models/Station.cs
+++ Projekt2/Projekt2/Models/Station.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Projekt2.Models
@@ -11,6 +12,36 @@ namespace Projekt2.Models
         public List<Platform> Platforms { get; set; }
         public List<Junction> Junctions { get; set; }
         public List<Train> Trains { get; set; }
+        public bool Go { get; set; }
 
+        public Thread stationManager;
+        public List<Thread> trainThreads = new List<Thread>();
+        public TimeSpan arrivalTime = new TimeSpan(0,0,0,0,200);
+        public TimeSpan overTime = new TimeSpan(0,0,0,2);
+
+        public Station(int platforms, int tracks, int trains, int entryTracks)
+        {
+            // tu będziemy tworzyć najpierw  junctiony 
+            // potem tracki 
+            // potem platformy 
+            // a trainy to na początku symulacji postawimy na trackach wiec nie tu
+            // station magnager też do stworzenia 
+        }
+        public void StartSimulation()
+        {
+            // no i tu zaczynamy symulacje czyli stawaimy pociągi na torach i lista wątków pociągów
+        }
+        public void EndSimulation()
+        {
+            // tu kończymy symulacje, wszystkie wątki killujemy itd. 
+        }
+        public void GenerateTrain()
+        {
+            // tu generujemy losowo jakiś pociąg na torze jeśli jest wolne więcej niż 1
+        }
+        public void StationManaging()
+        {
+            // tu generujemy losowo jakiś pociąg na torze jeśli jest wolne więcej niż 1
+        }
     }
 }
